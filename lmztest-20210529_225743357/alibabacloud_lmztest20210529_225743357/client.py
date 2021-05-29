975a21f2e5aba3bee1fe6f4dd44c7e9d88ea5fda
sdk-team <sdk-team@alibabacloud.com>
2021-05-29 16:52:51 +0000

    Amp.

---
+++ lmztest-20210529_225743357/alibabacloud_lmztest20210529_225743357/client.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from __future__ import unicode_literals
+
+from Tea.core import TeaCore
+
+from alibabacloud_tea_openapi.client import Client as OpenApiClient
+from alibabacloud_tea_openapi import models as open_api_models
+from alibabacloud_tea_util.client import Client as UtilClient
+from alibabacloud_endpoint_util.client import Client as EndpointUtilClient
+from alibabacloud_lmztest20210529_225743357 import models as lmz_test_20210529__225743357_models
+from alibabacloud_tea_util import models as util_models
+
+
+class Client(OpenApiClient):
+    """
+    *\
+    """
+    def __init__(self, config):
+        super(Client, self).__init__(config)
+        self._endpoint_rule = ''
+        self.check_config(config)
+        self._endpoint = self.get_endpoint('lmztest', self._region_id, self._endpoint_rule, self._network, self._suffix, self._endpoint_map, self._endpoint)
+
+    def get_endpoint(self, product_id, region_id, endpoint_rule, network, suffix, endpoint_map, endpoint):
+        if not UtilClient.empty(endpoint):
+            return endpoint
+        if not UtilClient.is_unset(endpoint_map) and not UtilClient.empty(endpoint_map.get(region_id)):
+            return endpoint_map.get(region_id)
+        return EndpointUtilClient.get_endpoint_rules(product_id, region_id, endpoint_rule, network, suffix)
+
+    def yx_test_api_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lmz_test_20210529__225743357_models.YxTestApiResponse(),
+            self.do_rpcrequest('YxTestApi', '2021-05-29_22-57-43-357', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def yx_test_api(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.yx_test_api_with_options(request, runtime)
