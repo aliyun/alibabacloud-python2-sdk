260fd9398a50eec03c414fb110c40773f2571ad0
sdk-team <sdk-team@alibabacloud.com>
2021-08-10 03:17:43 +0000

    Add GetScenePreviewInfo Interface.

--- tdsr-20200101/alibabacloud_tdsr20200101/client.py
+++ tdsr-20200101/alibabacloud_tdsr20200101/client.py
@@ -634,6 +634,20 @@ class Client(OpenApiClient):
         runtime = util_models.RuntimeOptions()
         return self.get_origin_layout_data_with_options(request, runtime)
 
+    def get_hotspot_scene_data_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            tdsr_20200101_models.GetHotspotSceneDataResponse(),
+            self.do_rpcrequest('GetHotspotSceneData', '2020-01-01', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_hotspot_scene_data(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_hotspot_scene_data_with_options(request, runtime)
+
     def scene_publish_with_options(self, request, runtime):
         UtilClient.validate_model(request)
         req = open_api_models.OpenApiRequest(
@@ -717,6 +731,20 @@ class Client(OpenApiClient):
     def get_policy(self, request):
         runtime = util_models.RuntimeOptions()
         return self.get_policy_with_options(request, runtime)
+
+    def get_scene_preview_info_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            tdsr_20200101_models.GetScenePreviewInfoResponse(),
+            self.do_rpcrequest('GetScenePreviewInfo', '2020-01-01', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_scene_preview_info(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_scene_preview_info_with_options(request, runtime)
 
     def add_sub_scene_with_options(self, request, runtime):
         UtilClient.validate_model(request)
