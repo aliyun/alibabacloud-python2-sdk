1b5079a9786e40be48a29f23236b4c713f274d52
sdk-team <sdk-team@alibabacloud.com>
2022-06-15 12:28:45 +0000

    Fix missing ListInstances.

--- fc-open-20210406/setup.py
+++ fc-open-20210406/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_fc-open20210406_py2.
 
-Created on 10/06/2022
+Created on 15/06/2022
 
 @author: Alibaba Cloud SDK
 """
