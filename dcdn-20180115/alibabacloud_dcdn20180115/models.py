abc5f19203205af6a094731f6e9c1b6ba0d0b6b8
sdk-team <sdk-team@alibabacloud.com>
2022-09-01 02:02:56 +0000

    Add DescribeDcdnL2Vips.

--- dcdn-20180115/alibabacloud_dcdn20180115/models.py
+++ dcdn-20180115/alibabacloud_dcdn20180115/models.py
@@ -16980,6 +16980,103 @@ class DescribeDcdnIpaUserDomainsResponse(TeaModel):
         return self
 
 
+class DescribeDcdnL2VipsRequest(TeaModel):
+    def __init__(self, domain_name=None):
+        self.domain_name = domain_name  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(DescribeDcdnL2VipsRequest, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.domain_name is not None:
+            result['DomainName'] = self.domain_name
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('DomainName') is not None:
+            self.domain_name = m.get('DomainName')
+        return self
+
+
+class DescribeDcdnL2VipsResponseBody(TeaModel):
+    def __init__(self, domain_name=None, request_id=None, vips=None):
+        self.domain_name = domain_name  # type: str
+        self.request_id = request_id  # type: str
+        self.vips = vips  # type: list[str]
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(DescribeDcdnL2VipsResponseBody, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.domain_name is not None:
+            result['DomainName'] = self.domain_name
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        if self.vips is not None:
+            result['Vips'] = self.vips
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('DomainName') is not None:
+            self.domain_name = m.get('DomainName')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        if m.get('Vips') is not None:
+            self.vips = m.get('Vips')
+        return self
+
+
+class DescribeDcdnL2VipsResponse(TeaModel):
+    def __init__(self, headers=None, status_code=None, body=None):
+        self.headers = headers  # type: dict[str, str]
+        self.status_code = status_code  # type: int
+        self.body = body  # type: DescribeDcdnL2VipsResponseBody
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.status_code, 'status_code')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(DescribeDcdnL2VipsResponse, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.status_code is not None:
+            result['statusCode'] = self.status_code
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('statusCode') is not None:
+            self.status_code = m.get('statusCode')
+        if m.get('body') is not None:
+            temp_model = DescribeDcdnL2VipsResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
 class DescribeDcdnL2VipsByDomainRequest(TeaModel):
     def __init__(self, domain_name=None, owner_id=None):
         self.domain_name = domain_name  # type: str
