3fdf5a31cf9e6749d8138f03d7beec6b052f08b8
sdk-team <sdk-team@alibabacloud.com>
2022-09-30 08:07:55 +0000

    InternationalIdcard.

--- ocr-api-20210707/alibabacloud_ocr_api20210707/client.py
+++ ocr-api-20210707/alibabacloud_ocr_api20210707/client.py
@@ -1216,6 +1216,38 @@ class Client(OpenApiClient):
         runtime = util_models.RuntimeOptions()
         return self.recognize_idcard_with_options(request, runtime)
 
+    def recognize_international_idcard_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.country):
+            query['Country'] = request.country
+        if not UtilClient.is_unset(request.url):
+            query['Url'] = request.url
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query),
+            body=request.body,
+            stream=request.body
+        )
+        params = open_api_models.Params(
+            action='RecognizeInternationalIdcard',
+            version='2021-07-07',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            ocr_api_20210707_models.RecognizeInternationalIdcardResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def recognize_international_idcard(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.recognize_international_idcard_with_options(request, runtime)
+
     def recognize_invoice_with_options(self, request, runtime):
         UtilClient.validate_model(request)
         query = {}
--- ocr-api-20210707/setup.py
+++ ocr-api-20210707/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_ocr-api20210707_py2.
 
-Created on 23/08/2022
+Created on 30/09/2022
 
 @author: Alibaba Cloud SDK
 """
