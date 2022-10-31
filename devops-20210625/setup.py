f785074cf9c7fc73dce64bef219fb2b7b45684c8
sdk-team <sdk-team@alibabacloud.com>
2022-10-31 14:22:18 +0000

    Add CreateWorkitemComment and change some API params.

--- devops-20210625/setup.py
+++ devops-20210625/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_devops20210625_py2.
 
-Created on 29/10/2022
+Created on 31/10/2022
 
 @author: Alibaba Cloud SDK
 """
