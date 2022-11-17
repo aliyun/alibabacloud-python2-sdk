192b9d258ee94d91d4e9cb5ff522afaabbf39f76
sdk-team <sdk-team@alibabacloud.com>
2022-11-17 08:06:49 +0000

    RecognizeInternationalBusinessLicense.

--- ocr-api-20210707/alibabacloud_ocr_api20210707/models.py
+++ ocr-api-20210707/alibabacloud_ocr_api20210707/models.py
@@ -4155,6 +4155,118 @@ class RecognizeIdcardResponse(TeaModel):
         return self
 
 
+class RecognizeInternationalBusinessLicenseRequest(TeaModel):
+    def __init__(self, country=None, url=None, body=None):
+        self.country = country  # type: str
+        self.url = url  # type: str
+        self.body = body  # type: READABLE
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(RecognizeInternationalBusinessLicenseRequest, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.country is not None:
+            result['Country'] = self.country
+        if self.url is not None:
+            result['Url'] = self.url
+        if self.body is not None:
+            result['body'] = self.body
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Country') is not None:
+            self.country = m.get('Country')
+        if m.get('Url') is not None:
+            self.url = m.get('Url')
+        if m.get('body') is not None:
+            self.body = m.get('body')
+        return self
+
+
+class RecognizeInternationalBusinessLicenseResponseBody(TeaModel):
+    def __init__(self, code=None, data=None, message=None, request_id=None):
+        self.code = code  # type: str
+        self.data = data  # type: str
+        self.message = message  # type: str
+        self.request_id = request_id  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(RecognizeInternationalBusinessLicenseResponseBody, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.code is not None:
+            result['Code'] = self.code
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        return self
+
+
+class RecognizeInternationalBusinessLicenseResponse(TeaModel):
+    def __init__(self, headers=None, status_code=None, body=None):
+        self.headers = headers  # type: dict[str, str]
+        self.status_code = status_code  # type: int
+        self.body = body  # type: RecognizeInternationalBusinessLicenseResponseBody
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.status_code, 'status_code')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(RecognizeInternationalBusinessLicenseResponse, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.status_code is not None:
+            result['statusCode'] = self.status_code
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('statusCode') is not None:
+            self.status_code = m.get('statusCode')
+        if m.get('body') is not None:
+            temp_model = RecognizeInternationalBusinessLicenseResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
 class RecognizeInternationalIdcardRequest(TeaModel):
     def __init__(self, country=None, url=None, body=None):
         self.country = country  # type: str
