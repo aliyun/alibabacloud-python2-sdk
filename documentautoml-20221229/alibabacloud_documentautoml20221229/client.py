2c18d00f8f753ec0794175d766ec9dfdcd385880
sdk-team <sdk-team@alibabacloud.com>
2023-02-10 03:34:08 +0000

    DocumentInit.

---
+++ documentautoml-20221229/alibabacloud_documentautoml20221229/client.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from __future__ import unicode_literals
+
+from Tea.core import TeaCore
+
+from alibabacloud_tea_openapi.client import Client as OpenApiClient
+from alibabacloud_tea_openapi import models as open_api_models
+from alibabacloud_tea_util.client import Client as UtilClient
+from alibabacloud_endpoint_util.client import Client as EndpointUtilClient
+from alibabacloud_documentautoml20221229 import models as document_automl_20221229_models
+from alibabacloud_tea_util import models as util_models
+from alibabacloud_openapi_util.client import Client as OpenApiUtilClient
+
+
+class Client(OpenApiClient):
+    """
+    *\
+    """
+    def __init__(self, config):
+        super(Client, self).__init__(config)
+        self._endpoint_rule = ''
+        self.check_config(config)
+        self._endpoint = self.get_endpoint('documentautoml', self._region_id, self._endpoint_rule, self._network, self._suffix, self._endpoint_map, self._endpoint)
+
+    def get_endpoint(self, product_id, region_id, endpoint_rule, network, suffix, endpoint_map, endpoint):
+        if not UtilClient.empty(endpoint):
+            return endpoint
+        if not UtilClient.is_unset(endpoint_map) and not UtilClient.empty(endpoint_map.get(region_id)):
+            return endpoint_map.get(region_id)
+        return EndpointUtilClient.get_endpoint_rules(product_id, region_id, endpoint_rule, network, suffix)
+
+    def predict_classifier_model_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.auto_prediction):
+            query['AutoPrediction'] = request.auto_prediction
+        if not UtilClient.is_unset(request.classifier_id):
+            query['ClassifierId'] = request.classifier_id
+        if not UtilClient.is_unset(request.content):
+            query['Content'] = request.content
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='PredictClassifierModel',
+            version='2022-12-29',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            document_automl_20221229_models.PredictClassifierModelResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def predict_classifier_model(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.predict_classifier_model_with_options(request, runtime)
+
+    def predict_model_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.binary_to_text):
+            query['BinaryToText'] = request.binary_to_text
+        if not UtilClient.is_unset(request.content):
+            query['Content'] = request.content
+        if not UtilClient.is_unset(request.model_id):
+            query['ModelId'] = request.model_id
+        if not UtilClient.is_unset(request.model_version):
+            query['ModelVersion'] = request.model_version
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='PredictModel',
+            version='2022-12-29',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            document_automl_20221229_models.PredictModelResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def predict_model(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.predict_model_with_options(request, runtime)
+
+    def predict_template_model_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.content):
+            query['Content'] = request.content
+        if not UtilClient.is_unset(request.project_id):
+            query['ProjectId'] = request.project_id
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='PredictTemplateModel',
+            version='2022-12-29',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            document_automl_20221229_models.PredictTemplateModelResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def predict_template_model(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.predict_template_model_with_options(request, runtime)
---
+++ documentautoml-20221229/alibabacloud_documentautoml20221229/models.py
@@ -0,0 +1,344 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from Tea.model import TeaModel
+
+
+class PredictClassifierModelRequest(TeaModel):
+    def __init__(self, auto_prediction=None, classifier_id=None, content=None):
+        self.auto_prediction = auto_prediction  # type: bool
+        self.classifier_id = classifier_id  # type: str
+        self.content = content  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(PredictClassifierModelRequest, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.auto_prediction is not None:
+            result['AutoPrediction'] = self.auto_prediction
+        if self.classifier_id is not None:
+            result['ClassifierId'] = self.classifier_id
+        if self.content is not None:
+            result['Content'] = self.content
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('AutoPrediction') is not None:
+            self.auto_prediction = m.get('AutoPrediction')
+        if m.get('ClassifierId') is not None:
+            self.classifier_id = m.get('ClassifierId')
+        if m.get('Content') is not None:
+            self.content = m.get('Content')
+        return self
+
+
+class PredictClassifierModelResponseBody(TeaModel):
+    def __init__(self, code=None, data=None, message=None, request_id=None):
+        self.code = code  # type: int
+        self.data = data  # type: str
+        self.message = message  # type: str
+        # Id of the request
+        self.request_id = request_id  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(PredictClassifierModelResponseBody, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.code is not None:
+            result['Code'] = self.code
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        return self
+
+
+class PredictClassifierModelResponse(TeaModel):
+    def __init__(self, headers=None, status_code=None, body=None):
+        self.headers = headers  # type: dict[str, str]
+        self.status_code = status_code  # type: int
+        self.body = body  # type: PredictClassifierModelResponseBody
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.status_code, 'status_code')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(PredictClassifierModelResponse, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.status_code is not None:
+            result['statusCode'] = self.status_code
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('statusCode') is not None:
+            self.status_code = m.get('statusCode')
+        if m.get('body') is not None:
+            temp_model = PredictClassifierModelResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
+class PredictModelRequest(TeaModel):
+    def __init__(self, binary_to_text=None, content=None, model_id=None, model_version=None):
+        self.binary_to_text = binary_to_text  # type: bool
+        self.content = content  # type: str
+        self.model_id = model_id  # type: long
+        self.model_version = model_version  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(PredictModelRequest, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.binary_to_text is not None:
+            result['BinaryToText'] = self.binary_to_text
+        if self.content is not None:
+            result['Content'] = self.content
+        if self.model_id is not None:
+            result['ModelId'] = self.model_id
+        if self.model_version is not None:
+            result['ModelVersion'] = self.model_version
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('BinaryToText') is not None:
+            self.binary_to_text = m.get('BinaryToText')
+        if m.get('Content') is not None:
+            self.content = m.get('Content')
+        if m.get('ModelId') is not None:
+            self.model_id = m.get('ModelId')
+        if m.get('ModelVersion') is not None:
+            self.model_version = m.get('ModelVersion')
+        return self
+
+
+class PredictModelResponseBody(TeaModel):
+    def __init__(self, code=None, data=None, message=None, request_id=None):
+        self.code = code  # type: int
+        self.data = data  # type: str
+        self.message = message  # type: str
+        # Id of the request
+        self.request_id = request_id  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(PredictModelResponseBody, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.code is not None:
+            result['Code'] = self.code
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        return self
+
+
+class PredictModelResponse(TeaModel):
+    def __init__(self, headers=None, status_code=None, body=None):
+        self.headers = headers  # type: dict[str, str]
+        self.status_code = status_code  # type: int
+        self.body = body  # type: PredictModelResponseBody
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.status_code, 'status_code')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(PredictModelResponse, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.status_code is not None:
+            result['statusCode'] = self.status_code
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('statusCode') is not None:
+            self.status_code = m.get('statusCode')
+        if m.get('body') is not None:
+            temp_model = PredictModelResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
+class PredictTemplateModelRequest(TeaModel):
+    def __init__(self, content=None, project_id=None):
+        self.content = content  # type: str
+        self.project_id = project_id  # type: long
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(PredictTemplateModelRequest, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.content is not None:
+            result['Content'] = self.content
+        if self.project_id is not None:
+            result['ProjectId'] = self.project_id
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Content') is not None:
+            self.content = m.get('Content')
+        if m.get('ProjectId') is not None:
+            self.project_id = m.get('ProjectId')
+        return self
+
+
+class PredictTemplateModelResponseBody(TeaModel):
+    def __init__(self, code=None, data=None, message=None, request_id=None):
+        self.code = code  # type: str
+        self.data = data  # type: str
+        self.message = message  # type: str
+        # Id of the request
+        self.request_id = request_id  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(PredictTemplateModelResponseBody, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.code is not None:
+            result['Code'] = self.code
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        return self
+
+
+class PredictTemplateModelResponse(TeaModel):
+    def __init__(self, headers=None, status_code=None, body=None):
+        self.headers = headers  # type: dict[str, str]
+        self.status_code = status_code  # type: int
+        self.body = body  # type: PredictTemplateModelResponseBody
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.status_code, 'status_code')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(PredictTemplateModelResponse, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.status_code is not None:
+            result['statusCode'] = self.status_code
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('statusCode') is not None:
+            self.status_code = m.get('statusCode')
+        if m.get('body') is not None:
+            temp_model = PredictTemplateModelResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
