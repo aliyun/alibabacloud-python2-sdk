2511433625855a08b5f91276bd94dd08d06c8871
sdk-team <sdk-team@alibabacloud.com>
2023-02-21 15:09:23 +0000

    Update.

--- btripopen-20220520/alibabacloud_btripopen20220520/client.py
+++ btripopen-20220520/alibabacloud_btripopen20220520/client.py
@@ -2693,6 +2693,43 @@ class Client(OpenApiClient):
         headers = btrip_open_20220520_models.ProjectModifyHeaders()
         return self.project_modify_with_options(request, headers, runtime)
 
+    def query_reimbursement_order_with_options(self, request, headers, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.reimb_order_no):
+            query['reimb_order_no'] = request.reimb_order_no
+        if not UtilClient.is_unset(request.sub_corp_id):
+            query['sub_corp_id'] = request.sub_corp_id
+        real_headers = {}
+        if not UtilClient.is_unset(headers.common_headers):
+            real_headers = headers.common_headers
+        if not UtilClient.is_unset(headers.x_acs_btrip_corp_token):
+            real_headers['x-acs-btrip-corp-token'] = UtilClient.to_jsonstring(headers.x_acs_btrip_corp_token)
+        req = open_api_models.OpenApiRequest(
+            headers=real_headers,
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='QueryReimbursementOrder',
+            version='2022-05-20',
+            protocol='HTTPS',
+            pathname='/reimbursement/v1/order',
+            method='GET',
+            auth_type='AK',
+            style='ROA',
+            req_body_type='json',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            btrip_open_20220520_models.QueryReimbursementOrderResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def query_reimbursement_order(self, request):
+        runtime = util_models.RuntimeOptions()
+        headers = btrip_open_20220520_models.QueryReimbursementOrderHeaders()
+        return self.query_reimbursement_order_with_options(request, headers, runtime)
+
     def sync_single_user_with_options(self, tmp_req, headers, runtime):
         UtilClient.validate_model(tmp_req)
         request = btrip_open_20220520_models.SyncSingleUserShrinkRequest()
