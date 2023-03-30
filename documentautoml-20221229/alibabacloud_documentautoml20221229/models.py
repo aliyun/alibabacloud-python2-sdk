103888545f2b36e3c87d58082cffa04866ae71dc
sdk-team <sdk-team@alibabacloud.com>
2023-03-30 04:13:17 +0000

    DocumentInit.

--- documentautoml-20221229/alibabacloud_documentautoml20221229/models.py
+++ documentautoml-20221229/alibabacloud_documentautoml20221229/models.py
@@ -3,6 +3,238 @@
 from Tea.model import TeaModel
 
 
+class CreateModelAsyncPredictRequest(TeaModel):
+    def __init__(self, binary_to_text=None, content=None, model_id=None, model_version=None, service_name=None,
+                 service_version=None):
+        self.binary_to_text = binary_to_text  # type: bool
+        self.content = content  # type: str
+        self.model_id = model_id  # type: long
+        self.model_version = model_version  # type: str
+        self.service_name = service_name  # type: str
+        self.service_version = service_version  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(CreateModelAsyncPredictRequest, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.binary_to_text is not None:
+            result['BinaryToText'] = self.binary_to_text
+        if self.content is not None:
+            result['Content'] = self.content
+        if self.model_id is not None:
+            result['ModelId'] = self.model_id
+        if self.model_version is not None:
+            result['ModelVersion'] = self.model_version
+        if self.service_name is not None:
+            result['ServiceName'] = self.service_name
+        if self.service_version is not None:
+            result['ServiceVersion'] = self.service_version
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('BinaryToText') is not None:
+            self.binary_to_text = m.get('BinaryToText')
+        if m.get('Content') is not None:
+            self.content = m.get('Content')
+        if m.get('ModelId') is not None:
+            self.model_id = m.get('ModelId')
+        if m.get('ModelVersion') is not None:
+            self.model_version = m.get('ModelVersion')
+        if m.get('ServiceName') is not None:
+            self.service_name = m.get('ServiceName')
+        if m.get('ServiceVersion') is not None:
+            self.service_version = m.get('ServiceVersion')
+        return self
+
+
+class CreateModelAsyncPredictResponseBody(TeaModel):
+    def __init__(self, code=None, data=None, message=None, request_id=None):
+        self.code = code  # type: int
+        self.data = data  # type: str
+        self.message = message  # type: str
+        # Id of the request
+        self.request_id = request_id  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(CreateModelAsyncPredictResponseBody, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.code is not None:
+            result['Code'] = self.code
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        return self
+
+
+class CreateModelAsyncPredictResponse(TeaModel):
+    def __init__(self, headers=None, status_code=None, body=None):
+        self.headers = headers  # type: dict[str, str]
+        self.status_code = status_code  # type: int
+        self.body = body  # type: CreateModelAsyncPredictResponseBody
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.status_code, 'status_code')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(CreateModelAsyncPredictResponse, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.status_code is not None:
+            result['statusCode'] = self.status_code
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('statusCode') is not None:
+            self.status_code = m.get('statusCode')
+        if m.get('body') is not None:
+            temp_model = CreateModelAsyncPredictResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
+class GetModelAsyncPredictRequest(TeaModel):
+    def __init__(self, async_predict_id=None):
+        self.async_predict_id = async_predict_id  # type: long
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(GetModelAsyncPredictRequest, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.async_predict_id is not None:
+            result['AsyncPredictId'] = self.async_predict_id
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('AsyncPredictId') is not None:
+            self.async_predict_id = m.get('AsyncPredictId')
+        return self
+
+
+class GetModelAsyncPredictResponseBody(TeaModel):
+    def __init__(self, code=None, data=None, message=None, request_id=None):
+        self.code = code  # type: int
+        self.data = data  # type: str
+        self.message = message  # type: str
+        # Id of the request
+        self.request_id = request_id  # type: str
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(GetModelAsyncPredictResponseBody, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.code is not None:
+            result['Code'] = self.code
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        return self
+
+
+class GetModelAsyncPredictResponse(TeaModel):
+    def __init__(self, headers=None, status_code=None, body=None):
+        self.headers = headers  # type: dict[str, str]
+        self.status_code = status_code  # type: int
+        self.body = body  # type: GetModelAsyncPredictResponseBody
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.status_code, 'status_code')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(GetModelAsyncPredictResponse, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.status_code is not None:
+            result['statusCode'] = self.status_code
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('statusCode') is not None:
+            self.status_code = m.get('statusCode')
+        if m.get('body') is not None:
+            temp_model = GetModelAsyncPredictResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
 class PredictClassifierModelRequest(TeaModel):
     def __init__(self, auto_prediction=None, classifier_id=None, content=None):
         self.auto_prediction = auto_prediction  # type: bool
@@ -40,7 +272,7 @@ class PredictClassifierModelRequest(TeaModel):
 class PredictClassifierModelResponseBody(TeaModel):
     def __init__(self, code=None, data=None, message=None, request_id=None):
         self.code = code  # type: int
-        self.data = data  # type: str
+        self.data = data  # type: dict[str, any]
         self.message = message  # type: str
         # Id of the request
         self.request_id = request_id  # type: str
@@ -271,7 +503,7 @@ class PredictTemplateModelRequest(TeaModel):
 class PredictTemplateModelResponseBody(TeaModel):
     def __init__(self, code=None, data=None, message=None, request_id=None):
         self.code = code  # type: str
-        self.data = data  # type: str
+        self.data = data  # type: dict[str, any]
         self.message = message  # type: str
         # Id of the request
         self.request_id = request_id  # type: str
