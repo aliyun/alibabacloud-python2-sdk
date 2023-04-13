d643c6945feabece2b1e8e522718ec75a272f417
sdk-team <sdk-team@alibabacloud.com>
2023-04-13 09:42:43 +0000

    DescribeDomain add CertificateValid date.

--- cloudapi-20160714/setup.py
+++ cloudapi-20160714/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_cloudapi20160714_py2.
 
-Created on 23/03/2023
+Created on 13/04/2023
 
 @author: Alibaba Cloud SDK
 """
