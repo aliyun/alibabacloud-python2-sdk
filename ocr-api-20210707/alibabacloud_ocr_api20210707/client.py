9b96b9fcdaa9038d3803838335a7171539ed107b
sdk-team <sdk-team@alibabacloud.com>
2023-05-23 08:27:30 +0000

    RecognizeHkIdcard.

--- ocr-api-20210707/alibabacloud_ocr_api20210707/client.py
+++ ocr-api-20210707/alibabacloud_ocr_api20210707/client.py
@@ -1054,6 +1054,36 @@ class Client(OpenApiClient):
         runtime = util_models.RuntimeOptions()
         return self.recognize_general_with_options(request, runtime)
 
+    def recognize_hkidcard_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.url):
+            query['Url'] = request.url
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query),
+            body=request.body,
+            stream=request.body
+        )
+        params = open_api_models.Params(
+            action='RecognizeHKIdcard',
+            version='2021-07-07',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            ocr_api_20210707_models.RecognizeHKIdcardResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def recognize_hkidcard(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.recognize_hkidcard_with_options(request, runtime)
+
     def recognize_handwriting_with_options(self, request, runtime):
         UtilClient.validate_model(request)
         query = {}
