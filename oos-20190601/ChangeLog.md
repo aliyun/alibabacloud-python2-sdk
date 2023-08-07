05ff0154f720b9d7bd14e1abdfaa50be0236c5ae
sdk-team <sdk-team@alibabacloud.com>
2023-08-07 07:36:40 +0000

    Generated python2 2019-06-01 for oos.

--- oos-20190601/ChangeLog.md
+++ oos-20190601/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-08-07 Version: 1.0.2
+- Generated python2 2019-06-01 for oos.
+
 2023-07-28 Version: 1.0.1
 - PatchBaseline Support Tags.
 
--- oos-20190601/setup.py
+++ oos-20190601/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_oos20190601_py2.
 
-Created on 28/07/2023
+Created on 07/08/2023
 
 @author: Alibaba Cloud SDK
 """
