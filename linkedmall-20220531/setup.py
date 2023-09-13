a7d25ee9762fc0eef3a126f15a97e68e97c589f8
sdk-team <sdk-team@alibabacloud.com>
2023-09-13 12:08:15 +0000

    Generated python2 2022-05-31 for linkedmall.

--- linkedmall-20220531/setup.py
+++ linkedmall-20220531/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_linkedmall20220531_py2.
 
-Created on 12/09/2023
+Created on 13/09/2023
 
 @author: Alibaba Cloud SDK
 """
