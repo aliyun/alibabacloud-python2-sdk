035dc7467218c6462d53c62a759b3ef4741ae3c5
sdk-team <sdk-team@alibabacloud.com>
2023-10-20 02:40:31 +0000

    Generated python2 2018-10-12 for alimt.

--- alimt-20181012/setup.py
+++ alimt-20181012/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_alimt20181012_py2.
 
-Created on 12/08/2023
+Created on 20/10/2023
 
 @author: Alibaba Cloud SDK
 """
