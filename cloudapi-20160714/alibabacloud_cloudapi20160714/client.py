7223b50048a187e343aad1043868dae723e079e9
sdk-team <sdk-team@alibabacloud.com>
2023-10-24 17:11:05 +0000

    Generated python2 2016-07-14 for CloudAPI.

--- cloudapi-20160714/alibabacloud_cloudapi20160714/client.py
+++ cloudapi-20160714/alibabacloud_cloudapi20160714/client.py
@@ -4278,6 +4278,42 @@ class Client(OpenApiClient):
         runtime = util_models.RuntimeOptions()
         return self.describe_domain_with_options(request, runtime)
 
+    def describe_group_latency_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.end_time):
+            query['EndTime'] = request.end_time
+        if not UtilClient.is_unset(request.group_id):
+            query['GroupId'] = request.group_id
+        if not UtilClient.is_unset(request.security_token):
+            query['SecurityToken'] = request.security_token
+        if not UtilClient.is_unset(request.stage_name):
+            query['StageName'] = request.stage_name
+        if not UtilClient.is_unset(request.start_time):
+            query['StartTime'] = request.start_time
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='DescribeGroupLatency',
+            version='2016-07-14',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            cloud_api20160714_models.DescribeGroupLatencyResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def describe_group_latency(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.describe_group_latency_with_options(request, runtime)
+
     def describe_group_qps_with_options(self, request, runtime):
         UtilClient.validate_model(request)
         query = {}
@@ -4313,6 +4349,42 @@ class Client(OpenApiClient):
     def describe_group_qps(self, request):
         runtime = util_models.RuntimeOptions()
         return self.describe_group_qps_with_options(request, runtime)
+
+    def describe_group_traffic_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.end_time):
+            query['EndTime'] = request.end_time
+        if not UtilClient.is_unset(request.group_id):
+            query['GroupId'] = request.group_id
+        if not UtilClient.is_unset(request.security_token):
+            query['SecurityToken'] = request.security_token
+        if not UtilClient.is_unset(request.stage_name):
+            query['StageName'] = request.stage_name
+        if not UtilClient.is_unset(request.start_time):
+            query['StartTime'] = request.start_time
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='DescribeGroupTraffic',
+            version='2016-07-14',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            cloud_api20160714_models.DescribeGroupTrafficResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def describe_group_traffic(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.describe_group_traffic_with_options(request, runtime)
 
     def describe_history_apis_with_options(self, request, runtime):
         UtilClient.validate_model(request)
