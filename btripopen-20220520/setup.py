0e5c18be528e1d4924146a4b8c225da066f12d75
sdk-team <sdk-team@alibabacloud.com>
2023-11-08 03:05:37 +0000

    Generated python2 2022-05-20 for btripOpen.

--- btripopen-20220520/setup.py
+++ btripopen-20220520/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_btripopen20220520_py2.
 
-Created on 07/11/2023
+Created on 08/11/2023
 
 @author: Alibaba Cloud SDK
 """
