c0140633584a36f9d872c2d44013ab5d2f900973
sdk-team <sdk-team@alibabacloud.com>
2023-11-09 03:13:41 +0000

    Generated python2 2022-01-30 for avatar.

--- avatar-20220130/setup.py
+++ avatar-20220130/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_avatar20220130_py2.
 
-Created on 08/11/2023
+Created on 09/11/2023
 
 @author: Alibaba Cloud SDK
 """
