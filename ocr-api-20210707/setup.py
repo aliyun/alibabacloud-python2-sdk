e5a37e7067a1ce2263e8bfdb5a098fe92f3e9447
sdk-team <sdk-team@alibabacloud.com>
2023-11-21 15:44:14 +0000

    Generated python2 2021-07-07 for ocr-api.

--- ocr-api-20210707/setup.py
+++ ocr-api-20210707/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_ocr-api20210707_py2.
 
-Created on 07/09/2023
+Created on 21/11/2023
 
 @author: Alibaba Cloud SDK
 """
