ceda5dd3d0c1e0e95b81e333a9ef3e7fb9460f7a
sdk-team <sdk-team@alibabacloud.com>
2024-01-10 17:08:51 +0000

    Generated python2 2024-01-18 for YicConsole.

--- yicconsole-20240118/alibabacloud_yicconsole20240118/client.py
+++ yicconsole-20240118/alibabacloud_yicconsole20240118/client.py
@@ -30,6 +30,36 @@ class Client(OpenApiClient):
             return endpoint_map.get(region_id)
         return EndpointUtilClient.get_endpoint_rules(product_id, region_id, endpoint_rule, network, suffix)
 
+    def billing_process_message_with_options(self, request, headers, runtime):
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.body):
+            query['body'] = request.body
+        req = open_api_models.OpenApiRequest(
+            headers=headers,
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='BillingProcessMessage',
+            version='2024-01-18',
+            protocol='HTTPS',
+            pathname='/yic/yic-console/v1/billing/commands/lifecycle',
+            method='POST',
+            auth_type='AK',
+            style='ROA',
+            req_body_type='json',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            yic_console_20240118_models.BillingProcessMessageResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    def billing_process_message(self, request):
+        runtime = util_models.RuntimeOptions()
+        headers = {}
+        return self.billing_process_message_with_options(request, headers, runtime)
+
     def check_pay_order_with_options(self, request, headers, runtime):
         UtilClient.validate_model(request)
         req = open_api_models.OpenApiRequest(
--- yicconsole-20240118/alibabacloud_yicconsole20240118/models.py
+++ yicconsole-20240118/alibabacloud_yicconsole20240118/models.py
@@ -27,6 +27,121 @@ class LxPopCmd(TeaModel):
         return self
 
 
+class BillingProcessMessageRequest(TeaModel):
+    def __init__(self, body=None):
+        self.body = body  # type: LxPopCmd
+
+    def validate(self):
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(BillingProcessMessageRequest, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('body') is not None:
+            temp_model = LxPopCmd()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
+class BillingProcessMessageResponseBody(TeaModel):
+    def __init__(self, message=None, synchro=None, code=None, data=None, request_id=None, success=None):
+        self.message = message  # type: str
+        self.synchro = synchro  # type: bool
+        self.code = code  # type: str
+        self.data = data  # type: str
+        # Id of the request
+        self.request_id = request_id  # type: str
+        self.success = success  # type: bool
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super(BillingProcessMessageResponseBody, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.synchro is not None:
+            result['Synchro'] = self.synchro
+        if self.code is not None:
+            result['code'] = self.code
+        if self.data is not None:
+            result['data'] = self.data
+        if self.request_id is not None:
+            result['requestId'] = self.request_id
+        if self.success is not None:
+            result['success'] = self.success
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('Synchro') is not None:
+            self.synchro = m.get('Synchro')
+        if m.get('code') is not None:
+            self.code = m.get('code')
+        if m.get('data') is not None:
+            self.data = m.get('data')
+        if m.get('requestId') is not None:
+            self.request_id = m.get('requestId')
+        if m.get('success') is not None:
+            self.success = m.get('success')
+        return self
+
+
+class BillingProcessMessageResponse(TeaModel):
+    def __init__(self, headers=None, status_code=None, body=None):
+        self.headers = headers  # type: dict[str, str]
+        self.status_code = status_code  # type: int
+        self.body = body  # type: BillingProcessMessageResponseBody
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.status_code, 'status_code')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super(BillingProcessMessageResponse, self).to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.status_code is not None:
+            result['statusCode'] = self.status_code
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('statusCode') is not None:
+            self.status_code = m.get('statusCode')
+        if m.get('body') is not None:
+            temp_model = BillingProcessMessageResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
 class CheckPayOrderRequest(TeaModel):
     def __init__(self, body=None):
         self.body = body  # type: LxPopCmd
