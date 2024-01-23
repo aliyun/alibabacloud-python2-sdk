c40b47f4d6daaa5130a28428954992c52c266b43
sdk-team <sdk-team@alibabacloud.com>
2024-01-23 17:13:48 +0000

    Generated python2 2022-01-01 for VpcPeer.

--- vpcpeer-20220101/setup.py
+++ vpcpeer-20220101/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_vpcpeer20220101_py2.
 
-Created on 28/11/2023
+Created on 23/01/2024
 
 @author: Alibaba Cloud SDK
 """
