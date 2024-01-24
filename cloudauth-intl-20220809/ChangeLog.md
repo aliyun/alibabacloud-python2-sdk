b16d5d76206602ac1bcafe4c5805ba319e374aea
sdk-team <sdk-team@alibabacloud.com>
2024-01-24 17:15:33 +0000

    Generated python2 2022-08-09 for Cloudauth-intl.

--- cloudauth-intl-20220809/ChangeLog.md
+++ cloudauth-intl-20220809/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-01-24 Version: 1.0.2
+- Generated python2 2022-08-09 for Cloudauth-intl.
+
 2024-01-22 Version: 1.0.1
 - Generated python2 2022-08-09 for Cloudauth-intl.
 
--- cloudauth-intl-20220809/setup.py
+++ cloudauth-intl-20220809/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_cloudauth-intl20220809_py2.
 
-Created on 22/01/2024
+Created on 24/01/2024
 
 @author: Alibaba Cloud SDK
 """
