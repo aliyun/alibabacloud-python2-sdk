602d2d0f20353a506e1b448d9ce24c1b1d9a99be
sdk-team <sdk-team@alibabacloud.com>
2024-02-28 17:15:44 +0000

    Support API AttachAssetGroupToInstance.

--- ddosbgp-20180720/setup.py
+++ ddosbgp-20180720/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_ddosbgp20180720_py2.
 
-Created on 21/02/2024
+Created on 28/02/2024
 
 @author: Alibaba Cloud SDK
 """
