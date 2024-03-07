3d35866d0ac7a0b2c9fc2ad2c631126d6341c154
sdk-team <sdk-team@alibabacloud.com>
2024-03-07 12:56:26 +0000

    Support API ActualDeductResources.

--- intelligentcreation-20240118/setup.py
+++ intelligentcreation-20240118/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_intelligentcreation20240118_py2.
 
-Created on 06/03/2024
+Created on 07/03/2024
 
 @author: Alibaba Cloud SDK
 """
