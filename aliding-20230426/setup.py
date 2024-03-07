34e97c8aa808f79e933a738b768db3cc72b29d60
sdk-team <sdk-team@alibabacloud.com>
2024-03-07 05:02:09 +0000

    Support API CheckUserIsGroupMember.

--- aliding-20230426/setup.py
+++ aliding-20230426/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_aliding20230426_py2.
 
-Created on 01/03/2024
+Created on 07/03/2024
 
 @author: Alibaba Cloud SDK
 """
