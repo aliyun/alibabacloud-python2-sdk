70f074f4da69fea6c6d38fb2fceacbc82e178664
sdk-team <sdk-team@alibabacloud.com>
2024-03-21 06:50:42 +0000

    Generated python2 2023-06-21 for PaiFeatureStore.

--- paifeaturestore-20230621/setup.py
+++ paifeaturestore-20230621/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_paifeaturestore20230621_py2.
 
-Created on 20/03/2024
+Created on 21/03/2024
 
 @author: Alibaba Cloud SDK
 """
