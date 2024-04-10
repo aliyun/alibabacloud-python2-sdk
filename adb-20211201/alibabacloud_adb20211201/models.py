9b33ee7cd0c1975c1a8c5bf4966438b6f05dcc95
sdk-team <sdk-team@alibabacloud.com>
2024-04-10 17:11:08 +0000

    Update API DescribeApsResourceGroups: add param RegionId.

--- adb-20211201/alibabacloud_adb20211201/models.py
+++ adb-20211201/alibabacloud_adb20211201/models.py
@@ -6299,11 +6299,13 @@ class DescribeApsActionLogsResponse(TeaModel):
 
 
 class DescribeApsResourceGroupsRequest(TeaModel):
-    def __init__(self, dbcluster_id=None):
+    def __init__(self, dbcluster_id=None, region_id=None, workload_id=None):
         # The ID of the AnalyticDB for MySQL Data Lakehouse Edition (V3.0) cluster.
         # 
         # >  You can call the [DescribeDBClusters](~~612397~~) operation to query the IDs of all AnalyticDB for MySQL Data Lakehouse Edition (V3.0) clusters within a region.
         self.dbcluster_id = dbcluster_id  # type: str
+        self.region_id = region_id  # type: str
+        self.workload_id = workload_id  # type: str
 
     def validate(self):
         pass
@@ -6316,12 +6318,20 @@ class DescribeApsResourceGroupsRequest(TeaModel):
         result = dict()
         if self.dbcluster_id is not None:
             result['DBClusterId'] = self.dbcluster_id
+        if self.region_id is not None:
+            result['RegionId'] = self.region_id
+        if self.workload_id is not None:
+            result['WorkloadId'] = self.workload_id
         return result
 
     def from_map(self, m=None):
         m = m or dict()
         if m.get('DBClusterId') is not None:
             self.dbcluster_id = m.get('DBClusterId')
+        if m.get('RegionId') is not None:
+            self.region_id = m.get('RegionId')
+        if m.get('WorkloadId') is not None:
+            self.workload_id = m.get('WorkloadId')
         return self
 
 
