51fd99ef95edb88bb98aec1c013dd557d89d6d9e
sdk-team <sdk-team@alibabacloud.com>
2024-04-17 17:10:12 +0000

    Generated python2 iap_1.0 for AliGenie.

--- aligenie-iap_1_0/setup.py
+++ aligenie-iap_1_0/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_aligenieiap_1_0_py2.
 
-Created on 16/04/2024
+Created on 17/04/2024
 
 @author: Alibaba Cloud SDK
 """
