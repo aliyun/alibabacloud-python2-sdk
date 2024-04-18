3e499557b2d25556788d3db874672c7fe57a7a34
sdk-team <sdk-team@alibabacloud.com>
2024-04-18 17:12:38 +0000

    Update API DescribeDBClusterAttribute: update response param.

--- polardb-20170801/alibabacloud_polardb20170801/models.py
+++ polardb-20170801/alibabacloud_polardb20170801/models.py
@@ -7189,21 +7189,40 @@ class DescribeCharacterSetNameResponse(TeaModel):
 class DescribeClassListRequest(TeaModel):
     def __init__(self, commodity_code=None, master_ha=None, order_type=None, owner_account=None, owner_id=None,
                  region_id=None, resource_group_id=None, resource_owner_account=None, resource_owner_id=None):
-        # The maximum number of IOPS.
+        # The code of the commodity. Valid values:
+        # 
+        # *   polardb_sub: the subscription cluster in regions in the Chinese mainland
+        # *   polardb_sub \_intl: the subscription cluster in regions outside the Chinese mainland
+        # *   polardb_payg: the pay-as-you-go cluster in regions in the Chinese mainland
+        # *   polardb_payg_intl: the pay-as-you-go cluster in regions outside the Chinese mainland
+        # *   polardb_sub_jushita: the subscription cluster for CloudTmall
+        # *   polardb_payg_jushita: the pay-as-you-go cluster for CloudTmall
+        # *   polardb_sub_cainiao: the subscription cluster for Cainiao
+        # *   polardb_payg_cainiao: the pay-as-you-go cluster for Cainiao
+        # 
+        # > *   If you use an Alibaba Cloud account on the China site, you can view only the codes of the commodities that are available in the Chinese mainland.
+        # >*   If you are using an Alibaba Cloud international account, you can view only the codes of the commodities that are available outside the Chinese mainland.
+        # >*   If you use a CloudTmall account, you can view only the codes of the commodities that are available in CloudTmall.
+        # >*   If you use a Cainiao account, you can view only the codes of the commodities that are available in Cainiao.
         self.commodity_code = commodity_code  # type: str
         # The number of nodes. Valid values:
         # 
-        # *   single
-        # *   cluster
-        # *   all
+        # *   single: Standalone Edition.
+        # *   cluster: Cluster Edition.
+        # *   all: both Standalone Edition and Cluster Edition.
         self.master_ha = master_ha  # type: str
-        # The specification type of the cluster.
+        # The type of the order. Valid values:
+        # 
+        # *   BUY: The order is used to purchase a cluster.
+        # *   UPGRADE: The order is used to change the specifications of a cluster.
+        # *   RENEW: The order is used to renew a cluster.
+        # *   CONVERT: The order is used to change the billing method of a cluster.
         self.order_type = order_type  # type: str
         self.owner_account = owner_account  # type: str
         self.owner_id = owner_id  # type: long
-        # The number of CPU cores.
+        # The region ID of the cluster.
         self.region_id = region_id  # type: str
-        # The ID of the request.
+        # The ID of the resource group.
         self.resource_group_id = resource_group_id  # type: str
         self.resource_owner_account = resource_owner_account  # type: str
         self.resource_owner_id = resource_owner_id  # type: long
@@ -7390,13 +7409,9 @@ class DescribeClassListResponseBody(TeaModel):
     def __init__(self, items=None, region_id=None, request_id=None):
         # The specifications of the cluster.
         self.items = items  # type: list[DescribeClassListResponseBodyItems]
+        # The region ID of the cluster.
         self.region_id = region_id  # type: str
-        # The price.
-        # 
-        # Unit: cents (US dollars).
-        # 
-        # > *   If you set the CommodityCode parameter to a value that indicates the pay-as-you-go billing method, the ReferencePrice parameter specifies the hourly fee that you must pay.
-        # > *   If you set the CommodityCode parameter to a value that indicates the subscription billing method, the ReferencePrice parameter specifies the monthly fee that you must pay.
+        # The ID of the request.
         self.request_id = request_id  # type: str
 
     def validate(self):
@@ -8004,8 +8019,8 @@ class DescribeDBClusterAttributeResponseBody(TeaModel):
                  proxy_cpu_cores=None, proxy_serverless_type=None, proxy_standard_cpu_cores=None, proxy_status=None,
                  proxy_type=None, region_id=None, request_id=None, resource_group_id=None, sqlsize=None, serverless_type=None,
                  standby_hamode=None, storage_max=None, storage_pay_type=None, storage_space=None, storage_type=None,
-                 storage_used=None, strict_consistency=None, sub_category=None, tags=None, vpcid=None, v_switch_id=None,
-                 zone_ids=None):
+                 storage_used=None, strict_consistency=None, sub_category=None, support_instant_switch_with_imci=None,
+                 tags=None, vpcid=None, v_switch_id=None, zone_ids=None):
         self.ai_creating_time = ai_creating_time  # type: str
         # The information status of the AI node. Valid values:
         # 
@@ -8167,6 +8182,7 @@ class DescribeDBClusterAttributeResponseBody(TeaModel):
         # 
         # > This parameter is supported only for PolarDB for MySQL clusters of Cluster Edition.
         self.sub_category = sub_category  # type: str
+        self.support_instant_switch_with_imci = support_instant_switch_with_imci  # type: str
         # Details about the tags.
         self.tags = tags  # type: list[DescribeDBClusterAttributeResponseBodyTags]
         # The VPC ID of the cluster.
@@ -8296,6 +8312,8 @@ class DescribeDBClusterAttributeResponseBody(TeaModel):
             result['StrictConsistency'] = self.strict_consistency
         if self.sub_category is not None:
             result['SubCategory'] = self.sub_category
+        if self.support_instant_switch_with_imci is not None:
+            result['SupportInstantSwitchWithImci'] = self.support_instant_switch_with_imci
         result['Tags'] = []
         if self.tags is not None:
             for k in self.tags:
@@ -8415,6 +8433,8 @@ class DescribeDBClusterAttributeResponseBody(TeaModel):
             self.strict_consistency = m.get('StrictConsistency')
         if m.get('SubCategory') is not None:
             self.sub_category = m.get('SubCategory')
+        if m.get('SupportInstantSwitchWithImci') is not None:
+            self.support_instant_switch_with_imci = m.get('SupportInstantSwitchWithImci')
         self.tags = []
         if m.get('Tags') is not None:
             for k in m.get('Tags'):
@@ -22921,7 +22941,7 @@ class ModifyDBClusterMaintainTimeRequest(TeaModel):
 
 class ModifyDBClusterMaintainTimeResponseBody(TeaModel):
     def __init__(self, request_id=None):
-        # The ID of the request.
+        # The request ID.
         self.request_id = request_id  # type: str
 
     def validate(self):
@@ -24074,14 +24094,14 @@ class ModifyDBClusterStorageSpaceRequest(TeaModel):
         self.planned_start_time = planned_start_time  # type: str
         self.resource_owner_account = resource_owner_account  # type: str
         self.resource_owner_id = resource_owner_id  # type: long
-        # The new storage capacity. Unit: GB.
+        # The storage capacity that you can select when you change the cluster. Unit: GB.
         # 
-        # > Valid values for PolarDB for MySQL Standard Edition are 20 to 32000.
+        # >  You can set this parameter for PolarDB for MySQL clusters of Standard Edition to a value that ranges from 20 to 32000.
         self.storage_space = storage_space  # type: long
         # The category of the cluster. Default value: ON. Valid values:
         # 
         # *   **normal_exclusive**: dedicated
-        # *   **normal_general**: genera-purpose
+        # *   **normal_general**: general-purpose
         self.sub_category = sub_category  # type: str
 
     def validate(self):
@@ -25006,14 +25026,14 @@ class ModifyDBNodesClassRequest(TeaModel):
         self.owner_id = owner_id  # type: long
         # The latest start time to upgrade the specifications within the scheduled time period. Specify the time in the ISO 8601 standard in the `YYYY-MM-DDThh:mm:ssZ` format. The time must be in UTC.
         # 
-        # > *   The value of this parameter must be at least 30 minutes later than the value of PlannedStartTime.
-        # >*   By default, if you specify `PlannedStartTime` but do not specify PlannedEndTime, the latest start time of the task is set to a value that is calculated by using the following formula: `Value of PlannedEndTime + 30 minutes`. For example, if you set `PlannedStartTime` to `2021-01-14T09:00:00Z` and you do not specify PlannedEndTime, the latest start time of the task is set to `2021-01-14T09:30:00Z`.
+        # >*   The value of this parameter must be at least 30 minutes later than the value of PlannedStartTime.
+        # >*   By default, if you specify `PlannedStartTime` but do not specify PlannedEndTime, the latest start time of the task is set to `Value of PlannedEndTime + 30 minutes`. For example, if you set `PlannedStartTime` to `2021-01-14T09:00:00Z` and you do not specify PlannedEndTime, the latest start time of the task is `2021-01-14T09:30:00Z`.
         self.planned_end_time = planned_end_time  # type: str
         # The earliest start time to upgrade the specifications within the scheduled time period. Specify the time in the ISO 8601 standard in the `YYYY-MM-DDThh:mm:ssZ` format. The time must be in UTC.
         # 
         # > *   This parameter takes effect only when `ModifyType` is set to `Upgrade`.
-        # >*   The earliest start time of the task can be a point in time within the next 24 hours. For example, if the current time is `2021-01-14T09:00:00Z`, you can specify a point in the time range from `2021-01-14T09:00:00Z` to `2021-01-15T09:00:00Z`.
-        # >*   If this parameter is empty, the upgrade task is immediately performed.
+        # >*   The earliest start time of the task can be a point in time within the next 24 hours. For example, if the current time is `2021-01-14T09:00:00Z`, you can specify a point in the time that ranges from `2021-01-14T09:00:00Z` to `2021-01-15T09:00:00Z`.
+        # >*   If this parameter is left empty, the upgrade task is immediately performed.
         self.planned_start_time = planned_start_time  # type: str
         self.resource_owner_account = resource_owner_account  # type: str
         self.resource_owner_id = resource_owner_id  # type: long
