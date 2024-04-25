6498bc419286b8df64caca5fb2edabb4391dc948
sdk-team <sdk-team@alibabacloud.com>
2024-04-25 02:35:30 +0000

    Generated python2 2021-05-15 for cloudsso.

--- cloudsso-20210515/setup.py
+++ cloudsso-20210515/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_cloudsso20210515_py2.
 
-Created on 22/04/2024
+Created on 25/04/2024
 
 @author: Alibaba Cloud SDK
 """
